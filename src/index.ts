9356e32684529b44fe6bdaf60ea4ab1dcdabd6ef
Craig Miller <craigmiller160@gmail.com>
2020-12-29 16:57:57 -0500

    Added another TODO

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,7 @@ import { ApiConfig } from './types';
 // TODO need post, put, delete, and graphql
 // TODO need CSRF interceptor
 // TODO need lots of unit tests
+// TODO integrate multiple error handlers (401, standard)
 
 export const createApi = (config: ApiConfig) => {
     const instance: AxiosInstance = createInstance(config.baseURL, config.useCsrf ?? false);
