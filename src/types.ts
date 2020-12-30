21292b8701f858cd0a88ce656bad6d162de0aa2d
Craig Miller <craigmiller160@gmail.com>
2020-12-29 21:03:20 -0500

    Added TODOs

--- src/types.ts
+++ src/types.ts
@@ -9,6 +9,9 @@ export interface RequestConfig {
     suppressError?: SuppressErrorFn;
 }
 
+// TODO need 3 options, default, 401, and axios error
+// TODO maybe a single method that takes the error and a parameter that defines the type?
+// TODO or a wrapper object that defines the type and has the error... parent class and sub-classes? polymorphism?
 export interface ErrorHandlingConfig {
     defaultErrorHandler: (error: Error) => void;
     unauthorizedErrorHandler?: <R>(error: AxiosError<R>) => void;
