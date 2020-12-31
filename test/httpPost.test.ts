5797f66a65225fe7e4a05940bb2ba521e9a995c7
Craig <craigmiller160@gmail.com>
2020-12-31 11:46:39 -0500

    Stubbed out more tests

---
+++ test/httpPost.test.ts
@@ -0,0 +1,26 @@
+
+describe('HTTP POST', () => {
+    it('makes successful request without CSRF', () => {
+        throw new Error();
+    });
+
+    it('makes successful request with CSRF', () => {
+        throw new Error();
+    });
+
+    it('has error with CSRF preflight with error handler', () => {
+        throw new Error();
+    });
+
+    it('makes request with 500 error and error handler', () => {
+        throw new Error();
+    });
+
+    it('makes request with 500 error without error handler', () => {
+        throw new Error();
+    });
+
+    it('makes request with generic error and error handler', () => {
+        throw new Error();
+    });
+});
