97e253d2ed0a0d01fcc9f4ae42f919d261a324f6
Craig <craigmiller160@gmail.com>
2020-12-31 11:23:49 -0500

    Stubbed out tests for GET requests

--- src/methods/get.ts
+++ src/methods/get.ts
@@ -5,7 +5,7 @@ import { ErrorHandler } from '../core/errorHandling';
 export const get = <R>(instance: AxiosInstance, handleError?: ErrorHandler) =>
     (req: RequestConfig): Promise<AxiosResponse<R>> =>
         instance.get<R>(req.uri, req.config)
-            .catch((ex: Error) => {
+            .catch((ex: Error) => { // TODO this catch block is going to be the same for all requests... make it generic and shared
                 if (!req.suppressError?.(ex)) {
                     handleError?.(ex, req);
                 }
---
+++ test/httpGet.test.ts
@@ -0,0 +1,19 @@
+
+
+describe('HTTP GET', () => {
+    it('makes successful request', () => {
+        throw new Error();
+    });
+
+    it('makes request with 500 error and error handling', () => {
+        throw new Error();
+    });
+
+    it('makes request with 500 error without error handling', () => {
+        throw new Error();
+    });
+
+    it('makes request with generic error and error handling', () => {
+        throw new Error();
+    });
+});
