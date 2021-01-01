c43c690d6a7afd6706eb980771a56c5ae713e171
Craig <craigmiller160@gmail.com>
2021-01-01 10:15:02 -0500

    Stubbed out all graphql tests

--- test/graphql.test.ts
+++ test/graphql.test.ts
@@ -1,7 +1,35 @@
 import MockAdapter from 'axios-mock-adapter';
 
 describe('graphql', () => {
-    it('test', () => {
+    it('makes successful request without CSRF', () => {
+        throw new Error();
+    });
+
+    it('makes successful request with CSRF', () => {
+        throw new Error();
+    });
+
+    it('has error with CSRF preflight with error handler', () => {
+        throw new Error();
+    });
+
+    it('makes request with 500 error and error handler', () => {
+        throw new Error();
+    });
+
+    it('makes request with 500 error without error handler', () => {
+        throw new Error();
+    });
+
+    it('makes request with generic error and error handler', () => {
+        throw new Error();
+    });
+
+    it('makes request with 500 error, with error handler, with suppress error', () => {
+        throw new Error();
+    });
+
+    it('makes request with GraphQL Error and error handler', () => {
         throw new Error();
     });
 });
