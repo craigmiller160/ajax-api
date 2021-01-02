5f36164467d5232dc1bfa270777f5a718d73355c
Craig Miller <craigmiller160@gmail.com>
2021-01-02 14:34:13 -0500

    Setup mockCsrfPreflight for consuming apps to test with

---
+++ src/test-utils/mockCsrfPreflight.ts
@@ -0,0 +1,17 @@
+import MockAdapter from 'axios-mock-adapter';
+import { CSRF_HEADER } from '../utils/csrfConstants';
+
+export const mockCsrfToken = 'ABCDEFG';
+
+export const mockCsrfPreflight = (mockApi: MockAdapter, uri: string, body?: any) =>
+    mockApi.onOptions(uri, body)
+        .reply((config) => {
+            expect(config.headers[CSRF_HEADER]).toEqual('fetch');
+            return [
+                200,
+                null,
+                {
+                    [CSRF_HEADER]: mockCsrfToken
+                }
+            ]
+        });
