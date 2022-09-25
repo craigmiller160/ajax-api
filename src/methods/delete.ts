fe71dd90a5d0c738582a547b23b3f7a4c5a1f301
Craig Miller <craigmiller160@gmail.com>
2022-09-25 14:02:46 -0400

    Updating types and delete functionality

--- src/methods/delete.ts
+++ src/methods/delete.ts
@@ -1,10 +1,10 @@
 import { AxiosInstance, AxiosResponse } from 'axios';
 import { customizeError, ErrorHandler } from '../core/errorHandling';
-import { UriRequestConfig } from '../types';
+import { UriBodyRequestConfig } from '../types';
 
 export const doDelete =
 	(instance: AxiosInstance, handleError?: ErrorHandler) =>
-	<R>(req: UriRequestConfig): Promise<AxiosResponse<R>> =>
+	<R, B = unknown>(req: UriBodyRequestConfig<B>): Promise<AxiosResponse<R>> =>
 		instance.delete<R>(req.uri, req.config).catch((ex: Error) => {
 			const newError = customizeError(ex, req.errorCustomizer);
 			handleError?.(newError, req);
--- src/methods/put.ts
+++ src/methods/put.ts
@@ -4,7 +4,7 @@ import { UriBodyRequestConfig } from '../types';
 
 export const put =
 	(instance: AxiosInstance, handleError?: ErrorHandler) =>
-	<B, R>(req: UriBodyRequestConfig<B>): Promise<AxiosResponse<R>> =>
+	<R, B>(req: UriBodyRequestConfig<B>): Promise<AxiosResponse<R>> =>
 		instance.put<R>(req.uri, req.body, req.config).catch((ex: Error) => {
 			const newError = customizeError(ex, req.errorCustomizer);
 			handleError?.(newError, req);
