e791011415741f0eb71caf3a2ae761e052127409
Craig Miller <craigmiller160@gmail.com>
2023-01-13 17:11:43 -0500

    More token logic

--- src/methods/post.ts
+++ src/methods/post.ts
@@ -1,12 +1,29 @@
-import { AxiosInstance, AxiosResponse } from 'axios';
+import { AxiosInstance, AxiosRequestConfig, AxiosResponse } from 'axios';
 import { customizeError, ErrorHandler } from '../core/errorHandling';
 import { UriBodyRequestConfig } from '../types';
+import {
+	AUTHORIZATION_HEADER,
+	BEARER_TOKEN_KEY
+} from '../utils/commonConstants';
 
 export const post =
 	(instance: AxiosInstance, handleError?: ErrorHandler) =>
-	<R, B>(req: UriBodyRequestConfig<B>): Promise<AxiosResponse<R>> =>
-		instance.post<R>(req.uri, req.body, req.config).catch((ex: Error) => {
-			const newError = customizeError(ex, req.errorCustomizer);
-			handleError?.(newError, req);
-			return Promise.reject(newError);
-		});
+	<R, B>(req: UriBodyRequestConfig<B>): Promise<AxiosResponse<R>> => {
+		const authHeader = localStorage.getItem(BEARER_TOKEN_KEY)
+			? `Bearer ${localStorage.getItem(BEARER_TOKEN_KEY)}`
+			: '';
+		const config: AxiosRequestConfig = {
+			...(req.config ?? {}),
+			headers: {
+				...(req.config?.headers ?? {}),
+				[AUTHORIZATION_HEADER]: authHeader
+			}
+		};
+		return instance
+			.post<R>(req.uri, req.body, config)
+			.catch((ex: Error) => {
+				const newError = customizeError(ex, req.errorCustomizer);
+				handleError?.(newError, req);
+				return Promise.reject(newError);
+			});
+	};
