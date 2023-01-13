9eefe0090378ccea610ebeaccff2ececa7b27cb5
Craig Miller <craigmiller160@gmail.com>
2023-01-13 17:11:13 -0500

    Implementing more token logic

--- src/methods/delete.ts
+++ src/methods/delete.ts
@@ -1,17 +1,30 @@
-import { AxiosInstance, AxiosResponse } from 'axios';
+import { AxiosInstance, AxiosRequestConfig, AxiosResponse } from 'axios';
 import { customizeError, ErrorHandler } from '../core/errorHandling';
 import { UriBodyRequestConfig } from '../types';
+import {
+	AUTHORIZATION_HEADER,
+	BEARER_TOKEN_KEY
+} from '../utils/commonConstants';
 
 export const doDelete =
 	(instance: AxiosInstance, handleError?: ErrorHandler) =>
-	<R, B = unknown>(req: UriBodyRequestConfig<B>): Promise<AxiosResponse<R>> =>
-		instance
-			.delete<R>(req.uri, {
-				...req.config,
-				data: req.body
-			})
-			.catch((ex: Error) => {
-				const newError = customizeError(ex, req.errorCustomizer);
-				handleError?.(newError, req);
-				return Promise.reject(newError);
-			});
+	<R, B = unknown>(
+		req: UriBodyRequestConfig<B>
+	): Promise<AxiosResponse<R>> => {
+		const authHeader = localStorage.getItem(BEARER_TOKEN_KEY)
+			? `Bearer ${localStorage.getItem(BEARER_TOKEN_KEY)}`
+			: '';
+		const config: AxiosRequestConfig = {
+			...(req.config ?? {}),
+			headers: {
+				...(req.config?.headers ?? {}),
+				[AUTHORIZATION_HEADER]: authHeader
+			},
+			data: req.body
+		};
+		return instance.delete<R>(req.uri, config).catch((ex: Error) => {
+			const newError = customizeError(ex, req.errorCustomizer);
+			handleError?.(newError, req);
+			return Promise.reject(newError);
+		});
+	};
--- src/methods/get.ts
+++ src/methods/get.ts
@@ -1,12 +1,27 @@
-import { AxiosInstance, AxiosResponse } from 'axios';
+import { AxiosInstance, AxiosRequestConfig, AxiosResponse } from 'axios';
 import { UriRequestConfig } from '../types';
 import { customizeError, ErrorHandler } from '../core/errorHandling';
+import {
+	AUTHORIZATION_HEADER,
+	BEARER_TOKEN_KEY
+} from '../utils/commonConstants';
 
 export const get =
 	(instance: AxiosInstance, handleError?: ErrorHandler) =>
-	<R>(req: UriRequestConfig): Promise<AxiosResponse<R>> =>
-		instance.get<R>(req.uri, req.config).catch((ex: Error) => {
+	<R>(req: UriRequestConfig): Promise<AxiosResponse<R>> => {
+		const authHeader = localStorage.getItem(BEARER_TOKEN_KEY)
+			? `Bearer ${localStorage.getItem(BEARER_TOKEN_KEY)}`
+			: '';
+		const config: AxiosRequestConfig = {
+			...(req.config ?? {}),
+			headers: {
+				...(req.config?.headers ?? {}),
+				[AUTHORIZATION_HEADER]: authHeader
+			}
+		};
+		return instance.get<R>(req.uri, config).catch((ex: Error) => {
 			const newError = customizeError(ex, req.errorCustomizer);
 			handleError?.(newError, req);
 			return Promise.reject(newError);
 		});
+	};
