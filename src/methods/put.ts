7e8278de5379b740e2b33fe421f10ca21fa66ef5
Craig Miller <craigmiller160@gmail.com>
2023-01-13 17:12:06 -0500

    More token logic

--- src/methods/put.ts
+++ src/methods/put.ts
@@ -1,12 +1,27 @@
-import { AxiosInstance, AxiosResponse } from 'axios';
+import { AxiosInstance, AxiosRequestConfig, AxiosResponse } from 'axios';
 import { customizeError, ErrorHandler } from '../core/errorHandling';
 import { UriBodyRequestConfig } from '../types';
+import {
+	AUTHORIZATION_HEADER,
+	BEARER_TOKEN_KEY
+} from '../utils/commonConstants';
 
 export const put =
 	(instance: AxiosInstance, handleError?: ErrorHandler) =>
-	<R, B>(req: UriBodyRequestConfig<B>): Promise<AxiosResponse<R>> =>
-		instance.put<R>(req.uri, req.body, req.config).catch((ex: Error) => {
+	<R, B>(req: UriBodyRequestConfig<B>): Promise<AxiosResponse<R>> => {
+		const authHeader = localStorage.getItem(BEARER_TOKEN_KEY)
+			? `Bearer ${localStorage.getItem(BEARER_TOKEN_KEY)}`
+			: '';
+		const config: AxiosRequestConfig = {
+			...(req.config ?? {}),
+			headers: {
+				...(req.config?.headers ?? {}),
+				[AUTHORIZATION_HEADER]: authHeader
+			}
+		};
+		return instance.put<R>(req.uri, req.body, config).catch((ex: Error) => {
 			const newError = customizeError(ex, req.errorCustomizer);
 			handleError?.(newError, req);
 			return Promise.reject(newError);
 		});
+	};
