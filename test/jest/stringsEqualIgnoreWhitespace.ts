6b1f2b47d9569a64be7a38cdfb3443415816246a
Craig Miller <craigmiller160@gmail.com>
2023-07-23 15:57:55 -0400

    Progress-ish

---
+++ test/jest/stringsEqualIgnoreWhitespace.ts
@@ -0,0 +1,34 @@
+const matchers = {
+	stringsEqualIgnoreWhitespace(received: string, expected: string) {
+		// Type checks to support JavaScript
+		if (typeof received !== 'string') {
+			return {
+				message: () => 'Received value is not a string',
+				pass: false
+			};
+		}
+		if (typeof expected !== 'string') {
+			return {
+				message: () => 'Expected value is not a string',
+				pass: false
+			};
+		}
+
+		const receivedNoWhitespace = received.trim().replace(/\s/g, '');
+		const expectedNoWhitespace = expected.trim().replace(/\s/g, '');
+		try {
+			expect(receivedNoWhitespace).toEqual(expectedNoWhitespace);
+			return {
+				message: () => '',
+				pass: true
+			};
+		} catch (ex) {
+			return {
+				message: () => (ex as Error).message,
+				pass: false
+			};
+		}
+	}
+};
+
+expect.extend(matchers);
