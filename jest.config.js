ba0348ddbdca692f2bb98d54556513596d23e3f8
Craig Miller <craigmiller160@gmail.com>
2023-07-24 16:14:00 -0400

    Updated test config

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,9 @@ const merge = require('@craigmiller160/config-merge');
 const jestConfig = require('@craigmiller160/jest-config');
 const path = require('path');
 
-module.exports = merge(jestConfig, {
-	setupFilesAfterEnv: [path.join(process.cwd(), 'test', 'setupTests.ts')]
+const config = merge(jestConfig, {
+	setupFilesAfterEnv: [path.join(process.cwd(), 'test', 'setupTests.ts')],
+	moduleNameMapper: {
+		'^axios$': require.resolve('axios')
+	}
 });
