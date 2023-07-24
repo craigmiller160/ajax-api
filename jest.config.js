1d23b83986386e8dec07a7cb2c988033ae852a5a
Craig Miller <craigmiller160@gmail.com>
2023-07-24 16:14:11 -0400

    Fixed bug

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,7 @@ const merge = require('@craigmiller160/config-merge');
 const jestConfig = require('@craigmiller160/jest-config');
 const path = require('path');
 
-const config = merge(jestConfig, {
+module.exports = merge(jestConfig, {
 	setupFilesAfterEnv: [path.join(process.cwd(), 'test', 'setupTests.ts')],
 	moduleNameMapper: {
 		'^axios$': require.resolve('axios')
